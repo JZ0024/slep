ea8d0e69b4d8fe788b8974c85f16e0a698e2f434
Shana Mathew <shanmath@qti.qualcomm.com>
2019-07-20 01:09:37 -0700

    bose example

---
+++ data-example/build/generated/source/buildConfig/debug/com/bose/wearable/sample/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.bose.wearable.sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.bose.ar.DataExample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 40009;
+  public static final String VERSION_NAME = "4.0.9";
+  // Fields from default config.
+  public static final String FOCUS_MODE = "IGNORED";
+}
